fb39bf39492ecc822811e6dc8cc396ef16a3f753
AhmadAl-Jabi <ahm.aljabi@gmail.com>
2026-04-04 01:55:22 -0400

    Nice UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import LiveFeed from './components/LiveFeed'
 import ChatPanel from './components/ChatPanel'
+import TopBar from './components/TopBar'
 import './App.css'
 
 function App() {
+  const [feedActive, setFeedActive] = useState(false)
+  const [status, setStatus] = useState('idle')
+
   return (
     <div className="app">
-      <LiveFeed />
-      <ChatPanel />
+      <LiveFeed onFeedChange={setFeedActive} />
+      <div className="overlay-ui">
+        <TopBar feedActive={feedActive} status={status} />
+        <div className="overlay-body">
+          <ChatPanel onStatusChange={setStatus} />
+        </div>
+      </div>
     </div>
   )
 }
